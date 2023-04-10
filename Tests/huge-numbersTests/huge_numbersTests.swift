1f5ae242bf0bd09d67cb48fbaa49983da5cd42e1
RandomHashTags <ImRandomHashTags@gmail.com>
2023-04-10 17:46:14 -0500

    added some tests

--- Tests/huge-numbersTests/huge_numbersTests.swift
+++ Tests/huge-numbersTests/huge_numbersTests.swift
@@ -10,8 +10,21 @@ import XCTest
 
 final class huge_numbersTests: XCTestCase {
     func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
+        test_int()
+    }
+    
+    private func test_int() {
+        var integer:HugeInt = HugeInt("1234567891011121314151617181920")
+        let second_integer:HugeInt = HugeInt("-1234567891011121314151617181920")
+        XCTAssert(integer != second_integer)
+        XCTAssert((integer > integer) == false)
+        XCTAssert((integer < integer) == false)
+        XCTAssert(integer >= integer)
+        XCTAssert(integer <= integer)
+        XCTAssert(second_integer < integer)
+        XCTAssert(second_integer <= integer)
+        
+        XCTAssert(integer * 2 == HugeInt("2469135782022242628303234363840"))
+        XCTAssert(second_integer * 2 == HugeInt("-2469135782022242628303234363840"))
     }
 }
